1fbc79ea2ed60ed44556eea4f397f43c4926a422
Emily <emilyahmad77@gmail.com>
2026-01-19 14:38:22 -0500

    bruno mars theme plan

---
+++ grenade_screen.gd
@@ -0,0 +1,61 @@
+extends Node
+
+# idea: (grenade screen)
+
+# start with the intro:
+# objective: catch grenades,
+# jump in front of trains, etc.
+
+# end screen: character falls to ground
+# (from wherever player had them once they caught
+# the last item),
+# character looks more beaten up
+# little arrow or some kind of signal to move the
+# character (like a platform) to the right side
+# of the screen (using arrow keys)
+# character moves off screen to next scene
+
+
+# 24k magic idea:
+
+# character looks classy, signal to move arrow keys
+# right to jump into pile of gold
+# (looks like leprachaun)
+# jumping in catapults character into sky
+# (like in desert in mario)
+# new background: gold coins
+# catch martinis, gold bars, dollar bills, little yachts
+# 24k magic plays in background
+# after collecting:
+# character flies upwards from last place
+# player moved it to UFO that appears
+# (under blue spotlight), once centered under
+# character moves behind UFO (disappears)
+# UFO flies up (out of screen)
+# spotlight (screen turns black except for
+# little circle / like in cartoons)
+# or some other transition
+
+# world is ending scren
+# platformers
+# wasteland type
+# transition: UFO appears and drops player
+# player has to collect cowboy hats, lassos
+# (western theme: ribs)
+# at end, animation (like ending of mario levels
+# but instead of a castle it's a spaceship)
+# that goes up off screen
+# then the player gets jumpscared by an
+# end screen reminiscent of duaa's love
+# calculator game but with a blown up
+# image of bruno mars
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
