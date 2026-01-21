1a182e113bd60efbf7245fe48c86d38b2596294b
Emily <emilyahmad77@gmail.com>
2026-01-21 00:18:37 -0500

    HUD collect game

--- hud.gd
+++ hud.gd
@@ -1,7 +1,6 @@
 extends CanvasLayer
 
 signal start_game
-signal start_multiplayer_game
 signal stop_music_pressed
 signal pause_toggled
 
@@ -19,42 +18,20 @@ func show_game_over():
 	$Message.text = "Catch the Rats!"
 	$Message.show()
 	$StartButton.show()
-	$MultiplayerButton.show()
 	$ScoreLabel.text = "0"
 
 func _display_winner(text):
 	winner_text = text
 
-func show_multiplayer_game_over():
-	show_message(winner_text)
-	await $MessageTimer.timeout
-	await get_tree().create_timer(1).timeout
-	$Message.text = "Catch Rats!"
-	$Message.show()
-	$StartButton.show()
-	$MultiplayerButton.show()
-	$ScoreLabel.text = "0"
-
 func update_score(score):
 	$ScoreLabel.text = str(score)
 
-func update_player2_score(player2score):
-	$Player2ScoreLabel.text = str(player2score)
-
 func _on_start_button_pressed():
 	$StartButton.hide()
-	$MultiplayerButton.hide()
-	$Player2ScoreLabel.hide()
 	start_game.emit()
 
 func _on_message_timer_timeout():
 	$Message.hide()
-
-func _on_multiplayer_button_pressed():
-	$StartButton.hide()
-	$MultiplayerButton.hide()
-	$Player2ScoreLabel.show()
-	start_multiplayer_game.emit()
 
 func _on_mute_label_pressed():
 	stop_music_pressed.emit()
