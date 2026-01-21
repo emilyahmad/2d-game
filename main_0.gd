49c9ec4b9578d0e7945980b9d1b0e9eea0c6a626
Emily <emilyahmad77@gmail.com>
2026-01-20 23:27:02 -0500

    creating the mini games (linked to main screen

--- main_0.gd
+++ main_0.gd
@@ -2,16 +2,17 @@ extends Node2D
 
 @onready var background: TextureRect = $Background
 
-@onready var game2: TextureButton = $Game1
+@onready var game2: TextureButton = $Game2Btn
+@onready var game3: TextureButton = $Game3Btn
 
 var game2unlocked := false
 var game3unlocked := false
 
 #once unlocked, player clicks on icons (buttons)
 #& taken to new scenes
-const COLLECT_GAME_PATH = "res://Scenes/Collect-Game-Screen.tscn"
-const AVOID_GAME_PATH = "res://Scenes/Avoid-Game-Screen.tscn"
-const PLATFORM_GAME_PATH = "res://Scenes/Platform-Game-Screen.tscn"
+const COLLECT_GAME_PATH = "res://Scenes/collect_game.tscn"
+const AVOID_GAME_PATH = "res://Scenes/avoid_game.tscn"
+const PLATFORM_GAME_PATH = "res://Scenes/platform_game.tscn"
 
 #change intro background to show mini games
 const INTRO_BACKGROUND := preload("res://HUD/Backgrounds/IntroBackground.png")
@@ -29,13 +30,28 @@ func _ready() -> void:
 	background.texture = INTRO_BACKGROUND
 	
 	if game3unlocked:
-		pass
+		game2.texture_normal = game2isunlocked
+		game3.texture_normal = game3isunlocked
 	elif game2unlocked:
-		pass
+		game2.texture_normal = game2isunlocked
+		game3.texture_normal = game3islocked
 	else:
-		pass
+		game2.texture_normal = game2islocked
+		game3.texture_normal = game3islocked
 
 func _on_start_btn_pressed() -> void:
 #changes to lobby background shows
 #mini games that are unlocked
 	background.texture = LOBBY_BACKGROUND
+
+func _on_game_1_btn_pressed() -> void:
+	var new_scene_resource = load(COLLECT_GAME_PATH)
+	get_tree().change_scene_to_packed(new_scene_resource)
+
+func _on_game_2_btn_pressed() -> void:
+	var new_scene_resource = load(AVOID_GAME_PATH)
+	get_tree().change_scene_to_packed(new_scene_resource)
+
+func _on_game_3_btn_pressed() -> void:
+	var new_scene_resource = load(PLATFORM_GAME_PATH)
+	get_tree().change_scene_to_packed(new_scene_resource)
